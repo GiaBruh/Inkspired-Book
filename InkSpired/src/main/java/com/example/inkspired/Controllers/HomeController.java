8f79432e8090c84846643f07b5c2d2ca170df196
Dat Do Duc <datddce171299@fpt.edu.vn>
2023-10-03 12:07:14 +0700

    Created a Controllers Package with 3 Servlet classes (HomeController, LoginController, LogoutController) Updated web.xml file to map Servlet classes

---
+++ InkSpired/src/main/java/com/example/inkspired/Controllers/HomeController.java
@@ -0,0 +1,58 @@
+package com.example.inkspired.Controllers;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HomeController", value = "/")
+public class HomeController extends HttpServlet {
+
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        processRequest(request, response);
+        System.out.println("HomeController");
+//        request.getRequestDispatcher("/index.jsp").forward(request, response);
+        response.sendRedirect(getServletContext().getContextPath() + "/login");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
