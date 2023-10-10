ff39a84d025c37dfe9a565c0864a1bf4b65ff2fa
Dat Do Duc <datddce171299@fpt.edu.vn>
2023-10-10 13:04:47 +0700

    Updated Admin model in model package
    Deleted phone_number field

--- InkSpired/src/main/java/com/example/inkspired/model/Admin.java
+++ InkSpired/src/main/java/com/example/inkspired/model/Admin.java
@@ -4,17 +4,15 @@ public class Admin {
     private String username;
     private String password;
     private String email_address;
-    private String phone_number;
     private String full_name;
 
     public Admin() {
     }
 
-    public Admin(String username, String password, String email_address, String phone_number, String full_name) {
+    public Admin(String username, String password, String email_address, String full_name) {
         this.username = username;
         this.password = password;
         this.email_address = email_address;
-        this.phone_number = phone_number;
         this.full_name = full_name;
     }
 
@@ -42,14 +40,6 @@ public class Admin {
         this.email_address = email_address;
     }
 
-    public String getPhone_number() {
-        return phone_number;
-    }
-
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
     public String getFull_name() {
         return full_name;
     }
@@ -60,12 +50,6 @@ public class Admin {
 
     @Override
     public String toString() {
-        return "Admin{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", email_address='" + email_address + '\'' +
-                ", phone_number='" + phone_number + '\'' +
-                ", full_name='" + full_name + '\'' +
-                '}';
+        return "Admin{" + "username='" + username + '\'' + ", password='" + password + '\'' + ", email_address='" + email_address + '\'' + ", full_name='" + full_name + '\'' + '}';
     }
 }
