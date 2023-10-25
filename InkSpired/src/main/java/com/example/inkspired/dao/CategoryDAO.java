1d193954e6de5ea8757b1e818e5682937458188e
Mai Quang Vinh <vinhmqce170001@fpt.edu.vn>
2023-10-25 18:29:42 +0700

    Modify some class for Category DAO

--- InkSpired/src/main/java/com/example/inkspired/dao/CategoryDAO.java
+++ InkSpired/src/main/java/com/example/inkspired/dao/CategoryDAO.java
@@ -35,7 +35,7 @@ public class CategoryDAO implements DAO<Category> {
     }
 
     @Override
-    public List getAll() {
+    public List<Category> getAll() {
         ArrayList<Category> result = new ArrayList<>();
         String query = "SELECT * FROM public.category";
         try {
@@ -87,6 +87,16 @@ public class CategoryDAO implements DAO<Category> {
 
     @Override
     public void update(Category o) {
+        String query = "UPDATE public.category SET category_name = ? WHERE category_id = ?";
+        try{
+            ps = conn.prepareStatement(query);
+            ps.setString(1, o.getCategory_name());
+            ps.setInt(2, o.getCategory_id());
+            ps.executeUpdate();
+        }catch (Exception ex) {
+            Logger.getLogger(CategoryDAO.class.getName()).log(Level.SEVERE, "Failed to update category", ex.getMessage());
+
+        }
     }
 
     @Override
@@ -101,13 +111,72 @@ public class CategoryDAO implements DAO<Category> {
         }
     }
 
-
-    public static void main(String[] args) {
-        CategoryDAO dao = new CategoryDAO();
-        Category category = new Category();
-        category.setCategory_name("Porn");
-//        dao.add(category);
-        dao.delete(category);
-        System.out.println(dao.getAll());
+    public List<Category> getAllCategories() {
+        List<Category> result = new ArrayList<>();
+        String query = "SELECT c.category_id, c.category_name, COUNT(cb.book_id) AS number_of_usage " +
+                "FROM category c " +
+                "LEFT JOIN category_book cb ON c.category_id = cb.category_id " +
+                "GROUP BY c.category_id, c.category_name " +
+                "ORDER BY c.category_id";
+        try {
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Category category = new Category();
+                category.setCategory_id(rs.getInt("category_id"));
+                category.setCategory_name(rs.getString("category_name"));
+                category.setNumber_of_usage(rs.getInt("number_of_usage"));
+                result.add(category);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(CategoryDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
+
+    public Category selectCategoryByID(int categoryId) {
+        String query = "SELECT * FROM public.category where category_id = ?";
+        Category category = null;
+        try{
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, categoryId);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                category = new Category();
+                category.setCategory_id(rs.getInt("category_id"));
+                category.setCategory_name(rs.getString("category_name"));
+            }
+
+        }catch (Exception ex) {
+            Logger.getLogger(CategoryDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return category;
+    }
+
+    public void deleteCategoryByID(int categoryId) {
+        String query = "DELETE FROM public.category where category_id = ?";
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, categoryId);
+            ps.executeUpdate();
+        } catch (Exception ex) {
+            Logger.getLogger(CategoryDAO.class.getName()).log(Level.SEVERE, "Failed to delete category", ex.getMessage());
+        }
+    }
+    //make class test selectCategoryByID
+
+    public void addCategory(Category category) {
+        String query = "INSERT INTO public.category (category_id, category_name) VALUES (?, ?)";
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, category.getCategory_id());
+            ps.setString(2, category.getCategory_name());
+            ps.executeUpdate();
+        } catch (Exception ex) {
+            Logger.getLogger(CategoryDAO.class.getName()).log(Level.SEVERE, "Failed to insert new category", ex.getMessage());
+        }
+    }
+
+
+
 }
