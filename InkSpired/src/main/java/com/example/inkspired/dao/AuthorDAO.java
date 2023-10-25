ce61982c4ab7e7b61ed3ca60314e231993860df4
Mai Quang Vinh <vinhmqce170001@fpt.edu.vn>
2023-10-25 18:27:42 +0700

    Modify some class for Author DAO

--- InkSpired/src/main/java/com/example/inkspired/dao/AuthorDAO.java
+++ InkSpired/src/main/java/com/example/inkspired/dao/AuthorDAO.java
@@ -3,6 +3,7 @@ package com.example.inkspired.dao;
 import com.example.inkspired.dbconnection.PostgresqlConnection;
 import com.example.inkspired.model.Author;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,6 +46,32 @@ public class AuthorDAO implements DAO<Author> {
         return result;
     }
 
+    public List<Author> getAllAuthors() {
+        List<Author> authors = new ArrayList<>();
+        String query = "SELECT a.author_id, a.author_fullname, a.author_description, a.author_image, COUNT(b.book_id) as number_of_books " +
+                "FROM author a " +
+                "JOIN author_book ab ON a.author_id = ab.author_id " +
+                "JOIN book b ON ab.book_id = b.book_id " +
+                "GROUP BY a.author_id, a.author_fullname, a.author_description, a.author_image";
+        try {
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Author author = new Author();
+                author.setAuthor_id(rs.getInt("author_id"));
+                author.setAuthor_fullname(rs.getString("author_fullname"));
+                author.setAuthor_image(rs.getString("author_image"));
+                author.setAuthor_description(rs.getString("author_description"));
+                author.setNumber_of_books(rs.getInt("number_of_books"));
+                authors.add(author);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return authors;
+    }
+
     @Override
     public Optional<Author> get(int id) {
         String query = "SELECT * FROM public.author where author_id = ?";
@@ -66,14 +93,52 @@ public class AuthorDAO implements DAO<Author> {
         return Optional.empty();
     }
 
+    public Author selectAuthorByID(int id) {
+        String query = "SELECT * FROM public.author where author_id = ?";
+        Author author = null; // Initialize the author as null
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                author = new Author(); // Create an Author object
+                author.setAuthor_id(rs.getInt("author_id"));
+                author.setAuthor_fullname(rs.getString("author_fullname"));
+                author.setAuthor_image(rs.getString("author_image"));
+                author.setAuthor_description(rs.getString("author_description"));
+                author.setAuthor_status(rs.getBoolean("author_status"));
+            }
+        } catch (Exception e) {
+            Logger.getLogger(AuthorDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return author; // Return the Author object (could be null if not found)
+    }
+
+
     @Override
     public void add(Author author) {
-        String query = "INSERT INTO public.author VALUES (?,?,?)";
+        String query = "INSERT INTO public.author (author_fullname, author_description, author_image, author_status) VALUES (?,?,?,?)";
         try {
             ps = conn.prepareStatement(query);
             ps.setString(1, author.getAuthor_fullname());
-            ps.setString(2, author.getAuthor_image());
+            ps.setString(2, author.getAuthor_description());
+            ps.setString(3, author.getAuthor_image());
+            ps.setBoolean(4, author.getAuthor_status());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            Logger.getLogger(AuthorDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    public void addAuthor(Author author) {
+        String query = "INSERT INTO public.author VALUES (?,?,?,?)";
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, author.getAuthor_id());
+            ps.setString(2, author.getAuthor_fullname());
             ps.setString(3, author.getAuthor_description());
+            ps.setString(4, author.getAuthor_image());
+
             ps.executeUpdate();
         } catch (Exception e) {
             Logger.getLogger(AuthorDAO.class.getName()).log(Level.SEVERE, null, e);
@@ -89,6 +154,21 @@ public class AuthorDAO implements DAO<Author> {
             ps.setString(2, author.getAuthor_description());
             ps.setString(3, author.getAuthor_image());
             ps.setInt(4, author.getAuthor_id());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            Logger.getLogger(AuthorDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    public void updateAuthor(Author author) {
+        String query = "UPDATE public.author SET author_fullname = ?,  author_description = ?, author_image = ?,author_status=? where author_id = ?";
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setString(1, author.getAuthor_fullname());
+            ps.setString(2, author.getAuthor_description());
+            ps.setString(3,"/"+ author.getAuthor_image());
+            ps.setBoolean(4, author.getAuthor_status());
+            ps.setInt(5, author.getAuthor_id());
             ps.executeUpdate();
         } catch (Exception e) {
             Logger.getLogger(AuthorDAO.class.getName()).log(Level.SEVERE, null, e);
@@ -118,4 +198,45 @@ public class AuthorDAO implements DAO<Author> {
         }
         return false;
     }
+
+    public List<Author> getBooksByAuthor() {
+        String query = "SELECT a.author_id, a.author_fullname, a.author_status, a.author_image, COUNT(ab.book_id) AS number_of_books " +
+                "FROM author a " +
+                "LEFT JOIN author_book ab ON a.author_id = ab.author_id " +
+                "GROUP BY a.author_id, a.author_fullname, a.author_image " +
+                "ORDER BY a.author_id";
+
+        List<Author> authors = new ArrayList<>();
+        try (PreparedStatement ps = conn.prepareStatement(query);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Author author = new Author();
+                author.setAuthor_id(rs.getInt("author_id"));
+                author.setAuthor_fullname(rs.getString("author_fullname"));
+                author.setAuthor_status(rs.getBoolean("author_status"));
+                author.setAuthor_image(rs.getString("author_image"));
+                author.setNumber_of_books(rs.getInt("number_of_books"));
+                authors.add(author);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Log any SQL-related exceptions
+        }
+        return authors;
+    }
+
+    public static void main(String[] args) {
+        AuthorDAO db = new AuthorDAO(); // Replace with your database class
+        List<Author> authors = db.getBooksByAuthor();
+
+        for (Author author : authors) {
+            System.out.println("Author ID: " + author.getAuthor_id());
+            System.out.println("Author Full Name: " + author.getAuthor_fullname());
+            System.out.println("Author Status: " + author.getAuthor_status());
+            System.out.println("Author Image: " + author.getAuthor_image());
+            System.out.println("Number of Books: " + author.getNumber_of_books());
+            System.out.println("-----------------------------");
+        }
+    }
+
+
 }
