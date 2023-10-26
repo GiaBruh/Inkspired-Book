72b8e422ae9f6a29384b8346640b2b45fa235878
Toriamal <longtlhce170879@fpt.edu.vn>
2023-10-26 08:59:26 +0700

    Update quantity-change.js
    For "Add to order" behavior

--- InkSpired/src/main/webapp/js/quantity-change.js
+++ InkSpired/src/main/webapp/js/quantity-change.js
@@ -12,3 +12,17 @@ $('.quantity button').on('click', function () {
     }
     button.parent().parent().find('input').val(newVal);
 });
+function AddTO(id) {
+    let btn = "btn_".concat(id)
+    if(document.getElementById(id).checked === false) {
+        document.getElementById(id).checked = true;
+        document.getElementById(btn).classList.remove('btn-dark');
+        document.getElementById(btn).classList.add('btn-light');
+        document.getElementById(btn).style.color = "green";
+    } else {
+        document.getElementById(id).checked = false;
+        document.getElementById(btn).classList.remove('btn-light');
+        document.getElementById(btn).classList.add('btn-dark');
+        document.getElementById(btn).style.color = "white";
+    }
+}
